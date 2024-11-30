019dbe0fdb832dff990a7f9b5a511f428756a36a
Yermek Orazgaliyev <yermek.orazgaliyev@smartgroups.io>
2024-11-30 19:53:43 +0500

    yay

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const axios = require("axios");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -24,39 +25,30 @@ const REDIRECT_URI = process.env.HOSTNAME + "/auth/callback";
 
 // Handle the OAuth redirect
 app.get("/auth/callback", async (req, res) => {
-    try {
-        // Get the code from the query parameters
-        const code = req.query.code;
+    res.redirect("https://www.instagram.com");
+    // try {
+    //     // Get the code from the query parameters
+    //     const code = req.query.code;
 
-        // Exchange the code for an access token
-        const tokenResponse = await axios.post("https://api.instagram.com/oauth/access_token", {
-            client_id: CLIENT_ID,
-            client_secret: CLIENT_SECRET,
-            grant_type: "authorization_code",
-            redirect_uri: REDIRECT_URI,
-            code: code,
-        });
+    //     // Exchange the code for an access token
+    //     const tokenResponse = await axios.post("https://api.instagram.com/oauth/access_token", {
+    //         client_id: CLIENT_ID,
+    //         client_secret: CLIENT_SECRET,
+    //         grant_type: "authorization_code",
+    //         redirect_uri: REDIRECT_URI,
+    //         code: code,
+    //     });
 
-        // Store the access token securely
-        const accessToken = tokenResponse.data.access_token;
-        // TODO: Save the access token to your database
+    //     // Store the access token securely
+    //     const accessToken = tokenResponse.data.access_token;
+    //     // TODO: Save the access token to your database
 
-        // Redirect user to success page
-        res.redirect("/success");
-    } catch (error) {
-        console.error("OAuth Error:", error);
-        res.redirect("/error");
-    }
-});
-
-// Success route
-app.get("/success", (req, res) => {
-    res.send("Authorization successful! You can close this window.");
-});
-
-// Error route
-app.get("/error", (req, res) => {
-    res.send("Authorization failed. Please try again.");
+    //     // Redirect user to success page
+    //     res.redirect("/success");
+    // } catch (error) {
+    //     console.error("OAuth Error:", error);
+    //     res.redirect("/error");
+    // }
 });
 
 app.listen(port);
