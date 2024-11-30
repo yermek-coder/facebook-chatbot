1c2e1459446ea64654da505ef68f47a752ec4df0
Yermek Orazgaliyev <yermek.orazgaliyev@smartgroups.io>
2024-11-30 19:40:43 +0500

    yay

--- index.js
+++ index.js
@@ -18,4 +18,45 @@ app.post("/", (req, res) => {
     res.send("Post req");
 });
 
+const CLIENT_ID = process.env.META_APP_ID;
+const CLIENT_SECRET = process.env.META_APP_SECRET;
+const REDIRECT_URI = process.env.HOSTNAME + "/auth/callback";
+
+// Handle the OAuth redirect
+app.get("/auth/callback", async (req, res) => {
+    try {
+        // Get the code from the query parameters
+        const code = req.query.code;
+
+        // Exchange the code for an access token
+        const tokenResponse = await axios.post("https://api.instagram.com/oauth/access_token", {
+            client_id: CLIENT_ID,
+            client_secret: CLIENT_SECRET,
+            grant_type: "authorization_code",
+            redirect_uri: REDIRECT_URI,
+            code: code,
+        });
+
+        // Store the access token securely
+        const accessToken = tokenResponse.data.access_token;
+        // TODO: Save the access token to your database
+
+        // Redirect user to success page
+        res.redirect("/success");
+    } catch (error) {
+        console.error("OAuth Error:", error);
+        res.redirect("/error");
+    }
+});
+
+// Success route
+app.get("/success", (req, res) => {
+    res.send("Authorization successful! You can close this window.");
+});
+
+// Error route
+app.get("/error", (req, res) => {
+    res.send("Authorization failed. Please try again.");
+});
+
 app.listen(port);
